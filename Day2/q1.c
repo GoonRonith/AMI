5dca917be31c5a38e2cb3683e7f29085e45a49d5
GoonRonith <ronith.goon@gmail.com>
2024-09-23 17:17:53 +0530

    assignment 1 of day2 updated

--- Day2/q1.c
+++ Day2/q1.c
@@ -4,29 +4,31 @@ int max(int x,int y){
     return y;
 }
 int main(){
-    int a[]={1,4,2,4,6};
-    int k=3;
-    int n=5;
-    int i,j=0;
+    int a[]={1,4,2,6,4}; 
+    int k=3;              
+    int n=5;              
     int preSum=0;
     int len=0;
-    int map[1000]={-1};
-    for(int i=1;i<1000;i++){
-        map[i]=-1;
+    int map[1000];        
+
+    
+    for(int i=0; i<1000; i++){
+        map[i] = -1;
     }
     
-    for(int i=0;i<n;i++){
-        preSum+=a[i];
-        int mod=preSum%k;
-        if(mod==0) len=i+1;
-        if(map[mod]!=-1){
-            len=max(len,i-map[mod]+1);
+    map[0] = 0;  
+
+    for(int i=0; i<n; i++){
+        preSum += a[i];
+        int mod = preSum % k;
+
+        if(map[mod] != -1){
+            len = max(len, i - map[mod] + 1);
+        } else {
+            map[mod] = i + 1;  
         }
-        if(map[preSum]==-1){
-            map[preSum]=i+1;
-        }
-        
     }
-    printf("%d",len);
     
+    printf("%d\n", len);  
+    return 0;
 }
