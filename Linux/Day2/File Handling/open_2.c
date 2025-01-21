02b43a7ad6db3b01db49193410f3586a1990aa3a
GoonRonith <ronith.goon@gmail.com>
2025-01-21 23:46:22 +0530

    modifications

--- Linux/Day2/File Handling/open_2.c
+++ Linux/Day2/File Handling/open_2.c
@@ -6,8 +6,8 @@
 
 void main(){
     int fd;
-    fd = open("newFile.log", O_RDWR | O_CREAT, S_IRWXU|S_IRWXG|S_IROTH);
-    //fd = open("newFile.log", O_RDWR | O_CREAT, 0774);
+    // fd = open("newFile.log", O_RDWR | O_CREAT, S_IRWXU|S_IRWXG|S_IROTH);
+    fd = open("newFile.log", O_RDWR | O_CREAT, 0774);
     if (fd == -1){
         printf("\n open() was failed - errno = (%d)\n",errno);
         perror("ERROR:");
